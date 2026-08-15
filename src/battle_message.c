40a276234c02b7d93d5c871af3367ba7f08a4040
monsterpeck <monkeyd2564@gmail.com>
2026-08-15 11:35:04 +0700

    battle: normalize invalid move type table initializer

--- src/battle_message.c
+++ src/battle_message.c
@@ -1344,7 +1344,7 @@ static const u8 sATypeMove_Table[NUMBER_OF_MON_TYPES][256] =
     [TYPE_PSYCHIC]  = _("a PSYCHIC move"),
     [TYPE_ICE]      = _("an ICE move"),
     [TYPE_DRAGON]   = _("a DRAGON move"),
-    [TYPE_DARK]     = _("a DARK move")
+    [TYPE_DARK]     = _("a DARK move"),
 };
 
 const u8 gText_BattleTourney[] = _("BATTLE TOURNEY");
